6e63d9ff80eaf995e553ec5101896856088e2549
Sabiner <chaoyue_ge@163.com>
2019-09-11 15:05:50 +0800

    Integration of the frontend

--- web/routers.go
+++ web/routers.go
@@ -4,11 +4,21 @@ import (
 	"Journey/app/integral"
 	"Journey/app/tag"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func InitRouter() * gin.Engine {
 	r := gin.New()
 	v1 := r.Group("/api/v1")
+
+	r.LoadHTMLGlob("frontend/**/*")
+	r.Static("/semantic", "semantic")
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "Journey",
+		})
+	})
 
 	integral.RegisterSubRouters(v1.Group("/integral"))
 	tag.RegisterSubRouters(v1.Group("/tag"))
